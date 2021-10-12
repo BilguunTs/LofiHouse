bd04480ed45ce4828023ae2561b78775b4477f28
Ponyo <usosketch@yahoo.com>
2021-10-12 19:06:50 +0800

    fixing fullscreen btn

--- Assets/Scripts/FullScreenBtn.cs
+++ Assets/Scripts/FullScreenBtn.cs
@@ -8,26 +8,32 @@ public class FullScreenBtn : MonoBehaviour
     private IconChanger Icons;
     private Image currentIcon;
     private Button thisBtn;
+    private bool isFullScreen;
     // Start is called before the first frame update
     void Start()
     {
         currentIcon = GetComponent<Image>();
         Icons = GetComponent<IconChanger>();
-        thisBtn.onClick.AddListener(TaskOnClick);
-        if (currentIcon.sprite == Icons.defaultImg)
+        thisBtn = GetComponent<Button>();
+        isFullScreen = Screen.fullScreen;
+        thisBtn.onClick.AddListener(delegate { TaskOnClick(); });
+        if (!isFullScreen)
         {
             Debug.Log("windowed mode");
-        }else if (currentIcon.sprite == Icons.activeImage)
+            currentIcon.sprite = Icons.defaultImg;
+        }else if (isFullScreen)
         {
             Debug.Log("full screen mode");
+            currentIcon.sprite = Icons.activeImage;
         }
     }
 
 
     void TaskOnClick()
     {
-        Screen.fullScreen = !Screen.fullScreen;
-        Screen.fullScreenMode= FullScreenMode.ExclusiveFullScreen;
-        thisBtn.GetComponent<Image>().sprite = !Screen.fullScreen ? Icons.activeImage : Icons.defaultImg;
+        isFullScreen = !isFullScreen;
+        Screen.fullScreen = isFullScreen;
+       //Screen.fullScreenMode= FullScreenMode.ExclusiveFullScreen;
+        currentIcon.sprite= !isFullScreen? Icons.defaultImg : Icons.activeImage;
     }
 }
